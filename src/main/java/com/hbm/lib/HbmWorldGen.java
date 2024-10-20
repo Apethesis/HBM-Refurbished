b6709414c1ddf9612412f7385e463c27402a6901
anonymousomeone <leoistheworst22@gmail.com>
2024-10-20 02:58:33 -0700

    when the fix is a break

--- src/main/java/com/hbm/lib/HbmWorldGen.java
+++ src/main/java/com/hbm/lib/HbmWorldGen.java
@@ -75,9 +75,9 @@ public class HbmWorldGen implements IWorldGenerator {
 	@Override
 	public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 		try{
-			generateOres(world, rand, chunkX + 8, chunkZ + 8);
+			generateOres(world, rand, chunkX * 16, chunkZ * 16);
 			if(world.getWorldInfo().isMapFeaturesEnabled())
-				generateStructures(world, rand, chunkX + 8, chunkZ + 8);
+				generateStructures(world, rand, chunkX * 16, chunkZ * 16);
 		} catch(final Throwable t){
 			System.out.println("NTM Worldgen Error "+t);
 			t.printStackTrace();
