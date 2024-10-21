40549d33737e0e24394a8093277352ea20159c48
anonymousomeone <leoistheworst22@gmail.com>
2024-10-20 18:32:39 -0700

    wtf?

--- src/main/java/com/hbm/inventory/ChemplantRecipes.java
+++ src/main/java/com/hbm/inventory/ChemplantRecipes.java
@@ -162,8 +162,8 @@ public class ChemplantRecipes {
 		makeRecipe(560, "ALGE", new AStack[] { new ComparableStack(ModItems.biomass, 4), new OreDictStack(KNO.dust(), 1), new OreDictStack(COAL.dustTiny(), 2) }, new FluidStack[]{ new FluidStack(FluidRegistry.WATER, 8000) }, new AStack[] { new ComparableStack(ModItems.biomass, 36) }, new FluidStack[]{ new FluidStack(FluidRegistry.WATER, 1000) }, 20*60);
 		
 		makeRecipe(570, "YELLOWCAKE", new AStack[] { new OreDictStack(U.billet(), 2), new OreDictStack(S.dust(), 2) }, new FluidStack[]{ new FluidStack(ModForgeFluids.acid, 500) }, new AStack[] { new ComparableStack(ModItems.powder_yellowcake, 1) }, null, 250);
-		
-		makeRecipe(580, "UF6", new AStack[] { new ComparableStack(ModItems.powder_yellowcake, 1), new OreDictStack(F.dust(), 4) }, new FluidStack[]{ new FluidStack(FluidRegistry.WATER, 1000) }, new AStack[] { new OreDictStack(S.dust(), 2) }, new FluidStack[]{ new FluidStack(ModForgeFluids.uf6, 1200) }, 100);
+
+		makeRecipe(580, "UF6", new AStack[] { new ComparableStack(ModItems.powder_yellowcake, 1), new OreDictStack(F.dust(), 4) }, new FluidStack[]{ new FluidStack(FluidRegistry.WATER, 1000) }, new AStack[] { new ComparableStack(ModItems.sulfur, 2) }, new FluidStack[]{ new FluidStack(ModForgeFluids.uf6, 1200) }, 100);
 
 		makeRecipe(590, "PUF6", new AStack[] { new OreDictStack(PU.dust(), 1), new OreDictStack(F.dust(), 3) }, new FluidStack[]{ new FluidStack(FluidRegistry.WATER, 1000) }, null, new FluidStack[]{ new FluidStack(ModForgeFluids.puf6, 900) }, 150);
 		
